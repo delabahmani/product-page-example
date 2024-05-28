9250ffa615ea66ff775dea2d8f45340490f4b1f2
delabahmani <delabahmani@gmail.com>
2024-05-27 21:05:43 -0400

    Complete navbar

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -4,14 +4,15 @@ const Navbar: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="relative flex items-center justify-between border border-black">
+    <nav className="relative lg:w-3/4 lg:mx-auto">
+      <div className="flex items-center justify-between py-5">
       <div className="block lg:hidden">
         <button
           onClick={() => setIsOpen(!isOpen)}
           className="flex items-center border-neutral-400 px-3 py-2 hover:border-white"
         >
           <svg
-            className="h-3 w-3 fill-current"
+            className="h-4 w-4 fill-current"
             viewBox="0 0 20 20"
             xmlns="http://www.w3.org/2000/svg"
           >
@@ -35,12 +36,12 @@ const Navbar: React.FC = () => {
         >
           <button
             onClick={() => setIsOpen(false)}
-            className="absolute left-5 top-0 mr-4 mt-4"
+            className="absolute left-6 top-1 mr-4 mt-4"
           >
             <img src="/assets/icon-close.svg" alt="" />
           </button>
 
-          <ul className="flex flex-col items-start px-5 py-20 justify-center space-y-4 font-bold text-lg">
+          <ul className="flex flex-col items-start justify-center space-y-4 px-6 py-20 text-lg font-bold">
             <li>Collections</li>
             <li>Men</li>
             <li>Women</li>
@@ -50,22 +51,34 @@ const Navbar: React.FC = () => {
         </div>
       </div>
 
-      <div className="mr-auto">
+      <div className="mr-auto flex">
         <img
           src="/assets/logo.svg"
           alt=""
-          className="mx-auto h-[23px] w-[140px]"
+          className="mx-auto h-[23px] w-[140px] mr-10"
         />
+
+        <div className="hidden lg:flex ">
+          <ul className="flex space-x-4 text-lg font-bold">
+            <li>Collections</li>
+            <li>Men</li>
+            <li>Women</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </div>
       </div>
 
-      <div className="ml-auto flex items-center space-x-2 py-">
+      <div className="ml-auto flex items-center space-x-5 px-6">
         <img
           src="/assets/icon-cart.svg"
           alt=""
-          className="mx-auto h-[20px] w-[20px]"
+          className="mx-auto h-[20px] w-[23px] lg:w-[28px] lg:h-[25px]"
         />
-        <img src="/assets/image-avatar.png" alt="" className="w-[30px]" />
+        <img src="/assets/image-avatar.png" alt="" className="w-[25px] lg:w-[50px]" />
       </div>
+      </div>
+      <hr className="text-[#f7f8fd] lg:w-full" />
     </nav>
   );
 };
